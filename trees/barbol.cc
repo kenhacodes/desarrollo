040e018532544dec057e9cd165043d77b2399138
KenHa <guillermobosca.bro@gmail.com>
2024-04-09 16:33:32 +0200

    trees done

--- trees/barbol.cc
+++ trees/barbol.cc
@@ -31,17 +31,46 @@ void paintLeaf(Tbarbol *tree, int BaseSpace, int depth){
   if(point->left != nullptr) paintLeaf(point->left, BaseSpace, ++depth);
 }
 
+
+
 void MostrarArbol(Tbarbol *tree, int BaseSpace){
   
   paintLeaf(tree, BaseSpace, 0);
   printf("\n------------ Other version ----------------\n");
 
-  int maxDepth = 0;
+  int maxDepth = 2;
   int depth = 0;
-  bool lookRight;
-  Tbarbol *point;
+  bool lookRight = true;
+  Tbarbol* point = tree;
+  
+  Tbarbol* level = (Tbarbol*) malloc(maxDepth * sizeof(Tbarbol));
+
+  while (point->left != nullptr && depth != maxDepth+1)
+  {    
+    while (point->right != nullptr)
+    {
+      *(level + depth) = *point;
+      depth++;
+      point = point->right;
+    };
+
+    for (int i = 1; i < BaseSpace + depth*3; i++) printf(" ");
+    printf("%03d\n", point->info);
+
+    depth--;
+    point = (level+depth);
+    
+    for (int i = 1; i < BaseSpace + depth*3; i++) printf(" ");
+    printf("%03d\n", point->info);
+
+    depth-=2;
+    point = (level+depth)->left;
+  }
+  
 
   
-    
+
+
+  
 }
 
